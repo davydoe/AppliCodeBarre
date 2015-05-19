eb0339fdd224d8c7ae0e8b1014bc33eafcfc520b
Juniors74335 <james.rudelin@gmail.com>
2015-05-19 00:18:11 -0700

    Mise en place du scan de code barre(plugnBarreCode)

--- FlashMe/platforms/android/assets/www/cordova_plugins.js
+++ FlashMe/platforms/android/assets/www/cordova_plugins.js
@@ -4,12 +4,20 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "com.phonegap.plugins.barcodescanner": "2.2.0"
 }
 // BOTTOM OF METADATA
 });
